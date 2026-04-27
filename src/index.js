02cdf419469d078e1f6df4f0e738044ee3bd4e91
admin <751379374@qq.com>
2026-04-27 10:12:21 +0800

    init log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const log=require('log4js')
-log.configure({
+const log4js=require('log4js')
+log4js.configure({
     appenders: { 
         OUT: { type: 'stdout' },
-        LOG: { type: 'file', filename: 'log.log' },
-        ERROR: { type: 'file', filename: 'error.log' }
+        LOG: { type: 'file', filename: 'log\\log.log' },
+        ERROR: { type: 'file', filename: 'log\\error.log' }
     },
     categories: {
         default: { appenders: ['OUT','LOG'], level: 'info' },
@@ -12,17 +12,20 @@ log.configure({
     }
 });
 
-const adapter = log.getLogger('cheese');
+const logERROR = log4js.getLogger('error');
+const logLOG = log4js.getLogger('default');
 const app = express()
+
 app.get('/', (req, res) => {
-    adapter.info('Received GET request');
+    logLOG.info('Received GET request');
     res.send('Hello World! 数据'+JSON.stringify(req.query));
 });
 app.post('/submit', (req, res) => {
-    adapter.info('Received POST request');
+    logLOG.info('Received POST request');
     res.send('提交成功！数据'+JSON.stringify(req.body));
 });
+
 app.listen(8888, () => {
-    adapter.info('Server is running on port 8888');
+    logLOG.info('Server is running on port 8888');
     console.log("端口号：8888");
 });
